625c3b451e9032a12d4589ec9cf6fee894337ed5
Matthew Leach <matt@fusefm.co.uk>
2010-09-06 23:55:07 +0100

    Create the database.

--- FuseUp.cpp
+++ FuseUp.cpp
@@ -4,6 +4,10 @@
 #include <QtGui/QMenu>
 #include <QtGui/QMenuBar>
 #include <QtGui/QAction>
+#include <QSqlDatabase>
+#include <QDir>
+#include <QMessageBox>
+#include <QSqlError>
 
 FuseUp::FuseUp()
 {
@@ -13,6 +17,16 @@ FuseUp::FuseUp()
   ui.AddButton->setShortcut(QKeySequence(Qt::Key_A));
   ui.EditButton->setShortcut(QKeySequence(Qt::Key_F2));
   ui.DeleteButton->setShortcut(QKeySequence(Qt::Key_Delete));
+  
+  // Create a database.
+  QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+  QString databasePath = QDir::homePath() + "//.member.db";
+  db.setDatabaseName(databasePath);
+  if(!db.open())
+  {
+    QMessageBox::critical(this, "Error opening database.", 
+			  "Error, could not open database: " + db.lastError().text());
+  }
 }
 
 FuseUp::~FuseUp()
