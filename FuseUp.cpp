f151e8cdd14c6ae120641b6f216eefa920cd1326
Matthew Leach <matt@fusefm.co.uk>
2010-09-06 23:21:06 +0100

    Added icons and show the main dialog.

--- FuseUp.cpp
+++ FuseUp.cpp
@@ -7,13 +7,7 @@
 
 FuseUp::FuseUp()
 {
-    QLabel* l = new QLabel( this );
-    l->setText( "Hello World!" );
-    setCentralWidget( l );
-    QAction* a = new QAction(this);
-    a->setText( "Quit" );
-    connect(a, SIGNAL(triggered()), SLOT(close()) );
-    menuBar()->addMenu( "File" )->addAction( a );
+  ui.setupUi(this);
 }
 
 FuseUp::~FuseUp()
--- FuseUp.h
+++ FuseUp.h
@@ -2,13 +2,16 @@
 #define FuseUp_H
 
 #include <QtGui/QMainWindow>
-
+#include "ui_main.h"
 class FuseUp : public QMainWindow
 {
 Q_OBJECT
 public:
     FuseUp();
     virtual ~FuseUp();
+    
+private:
+    Ui::MainWindow ui;
 };
 
 #endif // FuseUp_H
