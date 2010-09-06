b2a1b98098bdb32ae2ed7ac5123a6d65edef47d8
Matthew Leach <matt@fusefm.co.uk>
2010-09-06 23:34:51 +0100

    Setup shortcuts.

--- FuseUp.cpp
+++ FuseUp.cpp
@@ -8,6 +8,11 @@
 FuseUp::FuseUp()
 {
   ui.setupUi(this);
+  
+  // Setup shortcuts.
+  ui.AddButton->setShortcut(QKeySequence(Qt::Key_A));
+  ui.EditButton->setShortcut(QKeySequence(Qt::Key_F2));
+  ui.DeleteButton->setShortcut(QKeySequence(Qt::Key_Delete));
 }
 
 FuseUp::~FuseUp()
